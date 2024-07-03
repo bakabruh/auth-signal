dcc25a584d740002e9eefe574c0af3b546b854b9
Tom.L <tom.louveau@edu.devinci.fr>
2024-07-03 15:34:19 +0200

    ADD routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './features/authentication/authentication.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'authenticate',
+        children: authRoutes,
+    },
+    {
+        path: '**',
+        redirectTo: 'authenticate/login',
+        pathMatch: 'full'
+    },
+
+];
